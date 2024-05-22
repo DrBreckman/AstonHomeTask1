1c0d5a6c12f45a6bdf28da957efb91bb185f6a82
SedovNikita <vanoettt@mail.ru>
2024-05-22 19:16:47 +0300

    Added new comments

--- src/main/java/ru/sedov/implementations/MyEntry.java
+++ src/main/java/ru/sedov/implementations/MyEntry.java
@@ -2,6 +2,18 @@ package ru.sedov.implementations;
 
 import ru.sedov.MyMap;
 
+/**
+ * Класс, объекты которого используются в реализации MyHashMap. Объекты класса хранят
+ * пары ключ/значение, где ключ является константным и не может быть изменен. Класс
+ * предоставляет следующий набор методов для работы с его объектами:
+ * <ul>
+ *     <li>getKey() - получить ключ объекта</li>
+ *     <li>getValue() - получить значение объекта</li>
+ *     <li>setValue(key) - изменить значение объекта по заданному ключу</li>
+ * </ul>
+ * @param <K> тип данных ключа объекта
+ * @param <V> тип данных значения объекта
+ */
 public class MyEntry<K, V> implements MyMap.Entry<K, V> {
     private final K key;
     private V value;
