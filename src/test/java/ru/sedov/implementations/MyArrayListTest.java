1fb535be77ea17bc4e9f856b8e9c79c48e6dec3d
SedovNikita <vanoettt@mail.ru>
2024-05-22 20:06:29 +0300

    Reworked addByIndex in MyArrayList

--- src/test/java/ru/sedov/implementations/MyArrayListTest.java
+++ src/test/java/ru/sedov/implementations/MyArrayListTest.java
@@ -5,7 +5,6 @@ import ru.sedov.MyList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
